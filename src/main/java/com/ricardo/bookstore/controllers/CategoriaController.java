b9ef3239fbd260aa9d47530569d3fa3e6826dda4
ricardo.baccarini <ricardo.baccarini@gmail.com>
2021-07-02 17:47:40 -0300

    Adicionado o CategoriaDTO

--- src/main/java/com/ricardo/bookstore/controllers/CategoriaController.java
+++ src/main/java/com/ricardo/bookstore/controllers/CategoriaController.java
@@ -1,5 +1,8 @@
 package com.ricardo.bookstore.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -8,24 +11,32 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ricardo.bookstore.domain.Categoria;
+import com.ricardo.bookstore.dtos.CategoriaDTO;
 import com.ricardo.bookstore.service.CategoriaService;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriaController {
-	
+
 	@Autowired
 	private CategoriaService service;
-	
-    /*
-     * GetMapping(value = "/{id}") aqui o request para o findById
-     */
+
+	/*
+	 * GetMapping(value = "/{id}") aqui o request para o findById
+	 */
 	@GetMapping(value = "/{id}")
-	public ResponseEntity<Categoria> findById(@PathVariable Integer id){
+	public ResponseEntity<Categoria> findById(@PathVariable Integer id) {
 		Categoria obj = service.findById(id);
 		// Passa no corpo "body" o obj categoria
 		return ResponseEntity.ok().body(obj);
 	}
+
+	@GetMapping // como não tem argumento na busca faz o findAll
+	public ResponseEntity<List<CategoriaDTO>> findAll(){
+		List<Categoria> list = service.findAll();
+		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		}
 }
 
 // localhost:8080/categorias/1
