e14e6aaf87eab4dc810c28c4cadf76f7daf2e883
ricardo.baccarini <ricardo.baccarini@gmail.com>
2021-07-05 17:22:45 -0300

    Listando todos os livros por categoria.

--- src/main/java/com/ricardo/bookstore/controllers/LivroController.java
+++ src/main/java/com/ricardo/bookstore/controllers/LivroController.java
@@ -1,13 +1,18 @@
 package com.ricardo.bookstore.controllers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ricardo.bookstore.domain.Livro;
+import com.ricardo.bookstore.dtos.LivroDTO;
 import com.ricardo.bookstore.service.LivroService;
 
 @RestController
@@ -24,4 +29,13 @@ public class LivroController {
 		return ResponseEntity.ok().body(obj);
 	}
 
+	@GetMapping
+	public ResponseEntity<List<LivroDTO>> findAll(@RequestParam(value = "categoria", defaultValue = "0") Integer id_cat){
+		//localhost:8080/livro?categoria=1
+		
+		List<Livro> list = service.findAll(id_cat);
+		List<LivroDTO> listDTO = list.stream().map(obj -> new LivroDTO(obj)).collect(Collectors.toList());
+		return ResponseEntity.ok().body(listDTO);
+		
+	}
 }
