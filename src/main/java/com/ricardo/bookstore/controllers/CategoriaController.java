574b5992e218f84d799fc435258b23eb7f1b9a53
ricardo.baccarini <ricardo.baccarini@gmail.com>
2021-07-05 11:27:01 -0300

    Feito primeiro "Post" para adicionar a categoria (Insert) no banco de dados.

--- src/main/java/com/ricardo/bookstore/controllers/CategoriaController.java
+++ src/main/java/com/ricardo/bookstore/controllers/CategoriaController.java
@@ -1,5 +1,6 @@
 package com.ricardo.bookstore.controllers;
 
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -7,8 +8,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.ricardo.bookstore.domain.Categoria;
 import com.ricardo.bookstore.dtos.CategoriaDTO;
@@ -37,6 +41,13 @@ public class CategoriaController {
 		List<CategoriaDTO> listDTO = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
 		return ResponseEntity.ok().body(listDTO);
 		}
+	
+	@PostMapping
+	public ResponseEntity<Categoria> create(@RequestBody Categoria obj){
+		obj = service.create(obj);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("{/id}").buildAndExpand(obj.getId()).toUri();
+		return ResponseEntity.created(uri).build();
+	}
 }
 
 // localhost:8080/categorias/1
