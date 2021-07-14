314302d437b74e0d28c6a8a686ee9eb1a08900c6
ricardo.baccarini <ricardo.baccarini@gmail.com>
2021-07-14 18:14:22 -0300

    Adicionado classe de erro

--- src/main/java/com/ricardo/bookstore/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/ricardo/bookstore/controllers/exceptions/ControllerExceptionHandler.java
@@ -4,27 +4,40 @@ import javax.servlet.ServletRequest;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-import com.ricardo.bookstore.service.exceptions.ObjectNotFoundException;
 import com.ricardo.bookstore.service.exceptions.DataIntegrityViolationException;
+import com.ricardo.bookstore.service.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class) // Manipulador de exceção
 	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e, ServletRequest request) {
-		StandardError standarErro = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
 				e.getMessage());
-		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(standarErro);
+		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
 	}
 	
 	@ExceptionHandler(DataIntegrityViolationException.class) // Manipulador de exceção
 	public ResponseEntity<StandardError> dataIntegrityViolationException(DataIntegrityViolationException e, ServletRequest request) {
-		StandardError standarErro = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
 				e.getMessage());
-		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(standarErro);
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
-	
+
+	//MethodArgumentNotValidException
+	@ExceptionHandler(MethodArgumentNotValidException.class) // Manipulador de exceção
+	public ResponseEntity<StandardError> validationError(MethodArgumentNotValidException e, ServletRequest request) {
+		ValidationError error = new ValidationError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				"Erro na validação dos campos");
+		
+		for (FieldError x : e.getBindingResult().getFieldErrors()) {
+			error.addErrors(x.getField(), x.getDefaultMessage());
+		}
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
+	}
 }
