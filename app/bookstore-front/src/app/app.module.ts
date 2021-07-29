2ea37a6f90f2b103d09a6a1fe2507e5e9e4874ad
ricardo.baccarini <ricardo.baccarini@gmail.com>
2021-07-29 19:36:41 -0300

    Criado view, service, componente para listar todas as categorias, inserido baseUrl no environments.ts

--- app/bookstore-front/src/app/app.module.ts
+++ app/bookstore-front/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from "@angular/platform-browser";
 import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
 
 import { AppRoutingModule } from "./app-routing.module";
 import { AppComponent } from "./app.component";
@@ -14,9 +15,11 @@ import { MatListModule } from "@angular/material/list";
 import { MatIconModule } from "@angular/material/icon";
 import { HomeComponent } from './components/views/home/home.component';
 import {MatCardModule} from '@angular/material/card';
+import {MatTableModule} from '@angular/material/table';
+import { CategoriaReadComponent } from './components/views/categoria/categoria-read/categoria-read.component';
 
 @NgModule({
-  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent],
+  declarations: [AppComponent, HeaderComponent, FooterComponent, NavComponent, HomeComponent, CategoriaReadComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -26,6 +29,8 @@ import {MatCardModule} from '@angular/material/card';
     MatListModule,
     MatIconModule,
     MatCardModule,
+    MatTableModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent],
